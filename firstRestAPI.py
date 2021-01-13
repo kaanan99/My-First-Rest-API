6c3f913a3f8ac28b97bb614f18d439b281f66867
Kaanan Kharwa <kaanan99@gmail.com>
2021-01-13 12:46:36 -0800

    get user by id

--- firstRestAPI.py
+++ firstRestAPI.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
@@ -35,4 +36,20 @@ users = {
 }
 @app.route('/users')
 def get_useres():
+   search_username = request.args.get('name')
+   if search_username:
+      subdict = {'users_list':[]}
+      for user in users['users_list']:
+         if user['name']==search_username:
+            subdict['users_list'].append(user)
+      return subdict
+   return users
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id:
+      for user in users['users_list']:
+         if user['id'] == id:
+            return user
+      return ({})
    return users
